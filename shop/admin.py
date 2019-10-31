22aaa9a5fd1cb5a0ee6beca97dd21d37c080c354
Michał Kawa <michal.kawa@hotmail.com>
2019-10-31 20:24:24 +0100

    Category and product models created

---
+++ shop/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from shop.models import Product, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['category', 'name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created']
+    list_editable = ['price', 'available']  # editable columns from the level of list display
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ['category']

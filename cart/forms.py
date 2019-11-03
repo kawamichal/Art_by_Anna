46bc08688da00e0c25d1091aaf5c47345d94a614
kaweusz <michal.kawa@hotmail.com>
2019-11-03 23:37:39 +0100

    Cart form added

---
+++ cart/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
+
+
+class CartAddProductForm(forms.Form):
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
+                                      coerce=int)  # user can input quantity from 1 to 20, input is converted into int
+    update = forms.BooleanField(required=False, initial=False,
+                                widget=forms.HiddenInput)  # false - adding to the existing, true - replace the existing with new; widget will hide this choice

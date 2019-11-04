76404a48ec72957c1e667b73218b8faaf477751e
kaweusz <michal.kawa@hotmail.com>
2019-11-04 01:41:01 +0100

    Context processor for cart added

--- e_store/settings.py
+++ e_store/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
-    'cart.apps.CartConfig'
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -38,12 +38,13 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ['templates'],
         'APP_DIRS': True,
-        'OPTIONS': {
+        'OPTIONS': { # https://docs.djangoproject.com/en/2.2/ref/templates/api/#built-in-template-context-processors
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart' # custom context processor, returns cart to all templates
             ],
         },
     },

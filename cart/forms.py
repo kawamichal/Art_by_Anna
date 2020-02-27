f8e456f8e7f0a0f43eedd383a981ed53aedfdb6f
kawamichal <michal.kawa@hotmail.com>
2020-02-27 18:04:59 +0100

    Category, products and product detail menus corrected, buttons added

--- cart/forms.py
+++ cart/forms.py
@@ -5,6 +5,9 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
-                                      coerce=int)  # user can input quantity from 1 to 20, input is converted into int
+                                      coerce=int, widget=forms.Select(
+            attrs={
+                'style': 'background: none; padding: 2px',
+            }))  # user can input quantity from 1 to 20, input is converted into int
     update = forms.BooleanField(required=False, initial=False,
                                 widget=forms.HiddenInput)  # false - adding to the existing, true - replace the existing with new; widget will hide this choice
